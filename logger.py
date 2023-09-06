05a49e7d29b0683ae638f7d74c9d3e2bd6f153fe
x4080 <x4080@yahoo.com>
2023-09-06 07:56:17 +0700

    commit1

--- logger.py
+++ logger.py
@@ -6,12 +6,13 @@ import shutil
 class Logger(object):
     def __init__(self, log_dir, suffix=None):
         """Create a summary writer logging to log_dir."""
-        self.writer = tf.compat.v1.summary.FileWriter(log_dir, filename_suffix=suffix)
+        # self.writer = tf.summary.create_file_writer(log_dir)
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        # with self.writer.as_default():
+        #     tf.summary.scalar(tag, value, step=step)
+        #     self.writer.flush()
 
 
 class ModelLogger(object):
